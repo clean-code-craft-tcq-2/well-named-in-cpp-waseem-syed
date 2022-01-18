03ddd7bbae8511083dea330dad0673b3555f895d
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-01-18 23:21:57 +0530

    Add files via upload

--- TestColorPairCode.h
+++ TestColorPairCode.h
@@ -14,14 +14,14 @@ public:
 void TestColorPairCode::testNumberToPair(int pairNumber, TeleColorCoder::MajorColor expectedMajor, TeleColorCoder::MinorColor expectedMinor)
 {
     TeleColorCoder::ColorPair colorPair = TeleColorCoder::GetColorFromPairNumber(pairNumber);
-    //std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    //assert(colorPair.getMajor() == expectedMajor);
-    //assert(colorPair.getMinor() == expectedMinor);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
 }
 
 void TestColorPairCode::testPairToNumber(TeleColorCoder::MajorColor major, TeleColorCoder::MinorColor minor, int expectedPairNumber)
 {
    int pairNumber = TeleColorCoder::GetPairNumberFromColor(major, minor);
-   //std::cout << "Got pair number " << pairNumber << std::endl;
-   //assert(pairNumber == expectedPairNumber);
+   std::cout << "Got pair number " << pairNumber << std::endl;
+   assert(pairNumber == expectedPairNumber);
 }
