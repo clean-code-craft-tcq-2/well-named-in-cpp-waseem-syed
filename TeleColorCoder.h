e3d334acd7385e5e205b5e58fb5b22a825af2d6e
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-04-02 16:48:29 +0530

    Update TeleColorCoder.h

--- TeleColorCoder.h
+++ TeleColorCoder.h
@@ -1,6 +1,5 @@
 #ifndef TELECOLORCODER_H
 #define TELECOLORCODER_H
-
 #include <vector>
 #include <string>
 
@@ -8,10 +7,8 @@ namespace TeleCoColorCoder
 {
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-	std::vector<std::string > MajorColorNames {"White", "Red", "Black", "Yellow", "Violet"};
-	
-	std::vector<std::string > MinorColorNames {"Blue", "Orange", "Green", "Brown", "Slate"};
+    std::vector<std::string > MajorColorNames {"White", "Red", "Black", "Yellow", "Violet"};
+    std::vector<std::string > MinorColorNames {"Blue", "Orange", "Green", "Brown", "Slate"};
     int m_numberOfMinorColors = MinorColorNames.size();
 	
     class ColorPair {
@@ -19,50 +16,32 @@ namespace TeleCoColorCoder
             MajorColor m_majorColor;
             MinorColor m_minorColor;
         public:
-            ColorPair()
-			{
-			}
-			~ColorPair()
-			{
-			}
-            MajorColor getMajorColor()
-			{
-			   return m_majorColor;
-			}
-            MinorColor getMinorColor()
-			{
-			   return m_minorColor;
-			}
-		    void setMajorColor(MajorColor majorColor)
-			{
-			   m_majorColor = majorColor;
-			}
-            void setMinorColor(MinorColor minorColor)
-			{
-			   m_minorColor = minorColor;
-			}
-            std::string convertColorPairToString(ColorPair colorPair)
-			{
-			   std::string colorPairAsString = MajorColorNames[colorPair.getMajorColor()];
+            ColorPair(){}
+	   ~ColorPair(){}
+            MajorColor getMajorColor(){
+		return m_majorColor;}
+            MinorColor getMinorColor(){
+		return m_minorColor;}
+	   void setMajorColor(MajorColor majorColor){
+		m_majorColor = majorColor;}
+            void setMinorColor(MinorColor minorColor){
+		m_minorColor = minorColor;}
+            std::string convertColorPairToString(ColorPair colorPair){
+	       std::string colorPairAsString = MajorColorNames[colorPair.getMajorColor()];
                colorPairAsString += " ";
                colorPairAsString += MinorColorNames[colorPair.getMinorColor()];
-               return colorPairAsString;
-			}
+               return colorPairAsString;}
     };
 
-    ColorPair GetColorFromPairNumber(int pairNumber)
-	{
-	   ColorPair colorPair;
+    ColorPair GetColorFromPairNumber(int pairNumber){
+       ColorPair colorPair;
        int zeroBasedPairNumber = pairNumber - 1;
        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / m_numberOfMinorColors);
        colorPair.setMajorColor(majorColor);
        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % m_numberOfMinorColors);
        colorPair.setMinorColor(minorColor);
-       return colorPair;
-	}
-    int GetPairNumberFromColor(ColorPair colorPair)
-	{
-		return colorPair.getMajorColor() * m_numberOfMinorColors + colorPair.getMinorColor() + 1;
-	}
+       return colorPair;}
+    int GetPairNumberFromColor(ColorPair colorPair){
+	return colorPair.getMajorColor() * m_numberOfMinorColors + colorPair.getMinorColor() + 1;}
 }
-#endif		//TELECOLORCODER_H
+#endif		//TELECOLORCODER_H_
