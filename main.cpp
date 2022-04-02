71a8d38fb6d31f2d8de59b9621256c11f28dc6d5
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-04-02 17:11:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include "TestColorPairCode.h"
 
-using namespace TeleCoColorCoder;
+void displayColorCoding()
+{
+   for (int majorColorIndex = 0; majorColorIndex < MajorColorNames.size(); ++majorColorIndex)
+   {
+      for (int minorColorIndex = 0; minorColorIndex < MinorColorNames.size(); ++minorColorIndex)
+      {
+	  cout<< MajorColorNames[majorColorIndex]<<"\t"<<MinorColorNames[minorColorIndex]<<"\t\t"<<indexMajorColor + 1<<" "<< indexMinorColor + 1<<"\n";
+      }
+      cout<<"\n";
+   }
+}
 int main() {
-	TestColorPairCode testColorPairCode;
-
-    testColorPairCode.testNumberToPair(4, WHITE, BROWN);
-    testColorPairCode.testNumberToPair(5, WHITE, SLATE);
-
-    testColorPairCode.testPairToNumber(BLACK, ORANGE, 12);
-    testColorPairCode.testPairToNumber(VIOLET, SLATE, 25);
-
+    TestColorPairCode testColorPairCode;
+    testColorPairCode.testNumberToPair(4, TeleCoColorCoder::WHITE, TeleCoColorCoder::BROWN);
+    testColorPairCode.testNumberToPair(5, TeleCoColorCoder::WHITE, TeleCoColorCoder::SLATE);
+    testColorPairCode.testPairToNumber(TeleCoColorCoder::BLACK, TeleCoColorCoder::ORANGE, 12);
+    testColorPairCode.testPairToNumber(TeleCoColorCoder::VIOLET, TeleCoColorCoder::SLATE, 25);
+	
+    displayColorCoding();
     return 0;
 }
